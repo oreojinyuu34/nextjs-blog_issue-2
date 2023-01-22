9eeadb3f22d5a5041132dda36a5f53b18bfecdd1
atsushi <77239082+oreojinyuu34@users.noreply.github.com>
2023-01-22 16:37:47 +0900

    Next.JSのブログ_ファーストコミット

---
+++ components/Layout.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+
+const name = "oreoとatsushi";
+export const siteTitle = "Next.js blog";
+
+function Layout({ children }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        <img
+          src="/images/point.png"
+          className={utilStyles.borderCircle}
+          width={100}
+        />
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
