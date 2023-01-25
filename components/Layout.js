975b99ffffcc321fb6a69553c34e7acb164031e8
atsushi <77239082+oreojinyuu34@users.noreply.github.com>
2023-01-25 19:37:10 +0900

    マイクロブログ構築動的ルーティング

--- components/Layout.js
+++ components/Layout.js
@@ -1,25 +1,44 @@
 import Head from "next/head";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-const name = "oreoとatsushi";
+const name = "Oreo&Atsushi";
 export const siteTitle = "Next.js blog";
 
-function Layout({ children }) {
+function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        <img
-          src="/images/point.png"
-          className={utilStyles.borderCircle}
-          width={100}
-        />
+        {home ? (
+          <>
+            <img
+              src="/images/point.png"
+              className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`}
+              width={100}
+            />
+          </>
+        ) : (
+          <>
+            <img
+              src="/images/point.png"
+              className={`${utilStyles.borderCircle}`}
+              width={100}
+            />
+          </>
+        )}
+
         <h1 className={utilStyles.heading2Xl}>{name}</h1>
       </header>
       <main>{children}</main>
+      {!home && (
+        <div>
+          <Link href="/">ホームへに戻る</Link>
+        </div>
+      )}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
-import Layout from "@/components/Layout";
+import Layout, { siteTitle } from "@/components/Layout";
 import utilStyle from "../styles/utils.module.css";
 import { getPostsData } from "../lib/post";
 
 //SSGの場合
 export async function getStaticProps() {
   const allPostsData = getPostsData();
-  console.log(allPostsData);
+  // console.log(allPostsData);
 
   return {
     props: {
@@ -18,69 +18,40 @@ export async function getStaticProps() {
     },
   };
 }
+// //SSRの場合
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       //コンポーネントに渡すためのprops
+//     },
+//   };
+// }
 
 export default function Home({ allPostsData }) {
   return (
-    <Layout>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
       <section className={utilStyle.headingMd}>
         <p>プログラムの学習しています</p>
       </section>
 
       <section className={`${utilStyle.headingMd} ${utilStyle.padding1px}`}>
-        <h2>📝ブログ</h2>
+        {/* <h2>📝ブログ</h2> */}
         <div className={styles.grid}>
-          <article>
-            <Link href="/">
-              <img
-                src="/images/thumbnail01.jpg"
-                className={styles.thumbnailImage}
-              />
-            </Link>
-            <Link href="/">
-              <a className={utilStyle.boldText}>SSGとSSRの使い分け</a>
-              <br />
-            </Link>
-            <small className={utilStyle.lightText}>2023.1.22</small>
-          </article>
-          <article>
-            <Link href="/">
-              <img
-                src="/images/thumbnail01.jpg"
-                className={styles.thumbnailImage}
-              />
-            </Link>
-            <Link href="/">
-              <a className={utilStyle.boldText}>SSGとSSRの使い分け</a>
-              <br />
-            </Link>
-            <small className={utilStyle.lightText}>2023.1.22</small>
-          </article>
-          <article>
-            <Link href="/">
-              <img
-                src="/images/thumbnail01.jpg"
-                className={styles.thumbnailImage}
-              />
-            </Link>
-            <Link href="/">
-              <a className={utilStyle.boldText}>SSGとSSRの使い分け</a>
-              <br />
-            </Link>
-            <small className={utilStyle.lightText}>2023.1.22</small>
-          </article>
-          <article>
-            <Link href="/">
-              <img
-                src="/images/thumbnail01.jpg"
-                className={styles.thumbnailImage}
-              />
-            </Link>
-            <Link href="/">
-              <a className={utilStyle.boldText}>SSGとSSRの使い分け</a>
-              <br />
-            </Link>
-            <small className={utilStyle.lightText}>2023.1.22</small>
-          </article>
+          {allPostsData.map(({ id, title, date, thumbnail }) => (
+            <article key={id}>
+              <Link href={`/posts/${id}`}>
+                <img src={`${thumbnail}`} className={styles.thumbnailImage} />
+              </Link>
+              <Link href={`/posts/${id}`}>
+                <p className={utilStyle.boldText}>{title}</p>
+                <br />
+              </Link>
+              <small className={utilStyle.lightText}>{date}</small>
+            </article>
+          ))}
         </div>
       </section>
     </Layout>
